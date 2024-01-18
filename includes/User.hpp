700c2b1348ef2ebeea121f722a012c5ff13a9971
Lisa Ciullo <lciullo@z4r12p1.42lyon.fr>
2024-01-18 16:48:37 +0100

    add class user

--- includes/User.hpp
+++ includes/User.hpp
@@ -1,10 +1,15 @@
 #ifndef USER_HPP
-# define USER_HPP
+#define USER_HPP
 
 class User
 {
-    public :
-        User();
+	private :
+				std::string	_nickName;
+				std::string	_channel;
+				std::string	_userName;
+
+	public :
+				User();
 };
 
 #endif
