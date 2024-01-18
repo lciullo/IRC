29abf7945ab84af3e996f8206c97abcbf993f989
Lisa Ciullo <lciullo@z4r12p1.42lyon.fr>
2024-01-18 15:58:05 +0100

    add branch user and parse nb arg

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,8 @@ int main(int ac, char **av)
 {
 	if (ac != 3)
 	{
-		std::cout << "[ERROR] Not good number of arguments";
+		std::cout << RED << "[ERROR] Wrong number of arguments" << RESET << std::endl;
+		std::cout << GREEN << "Usage : ./ircserv <port> <password>" << RESET << std::endl;
 		return (1);
 	}
 	Server srv(atoi(av[1]), av[2]);
