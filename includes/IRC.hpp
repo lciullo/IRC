f9c916aad936de59e797099139dd59aad359c377
lciullo <lisa.ciullo42@gmail.com>
2024-01-18 19:19:16 +0100

    add class user

--- includes/IRC.hpp
+++ includes/IRC.hpp
@@ -4,6 +4,7 @@
 //======== LIBRAIRIES ==========//
 
 #include <iostream>
+#include <string>
 
 //======== DEFINE =============//
 
--- includes/User.hpp
+++ includes/User.hpp
@@ -1,15 +1,21 @@
 #ifndef USER_HPP
 #define USER_HPP
 
+#include "IRC.hpp"
+
 class User
 {
 	private :
-				std::string	_nickName;
+				std::string	_nickname;
 				std::string	_channel;
-				std::string	_userName;
+				std::string	_username;
+				User(void);
 
 	public :
-				User();
+				User(const std::string& nickname, const std::string& username);
+				~User(void);
+				const std::string& getNickname(void) const;
+				const std::string& getUsername(void) const; 
 };
 
 #endif
