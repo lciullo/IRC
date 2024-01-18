27abbcd0aff5f54250174866eb954f8028d031e4
Aymeric Jakubczyk <ajakubcz@z4r12p3.42lyon.fr>
2024-01-18 17:25:03 +0100

    added error handling on arguments

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,8 @@
 #include "IRC.hpp"
-
 #include "Server.hpp"
+#include <cstdlib>
+
+bool good_arg(char *port, char *address);
 
 int main(int ac, char **av)
 {
@@ -10,6 +12,32 @@ int main(int ac, char **av)
 		std::cout << GREEN << "Usage : ./ircserv <port> <password>" << RESET << std::endl;
 		return (1);
 	}
+	if (!good_arg(av[1], av[2]))
+	{
+		return (1);
+	}
 	Server srv(atoi(av[1]), av[2]);
 	srv.launch();
+}
+
+bool good_arg(char *port, char *address)
+{
+	(void) address;
+	int port_nbr;
+
+	for (int i = 0; port[i]; i++)
+	{
+		if (port[i] < '0' || port[i] > '9')
+		{
+			std::cout << RED << "[ERROR] Wrong port : \"" << port << "\", put only digit in port" << RESET << std::endl;
+			return false;
+		}
+	}
+	port_nbr = atoi(port);
+	if (port_nbr < 1024 || port_nbr > 65535)
+	{
+		std::cout << RED << "[ERROR] Wrong port : \"" << port << "\", put port between 1024 and 65535" << RESET << std::endl;
+		return false;
+	}
+	return true;
 }
