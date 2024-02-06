9dda7c8a2117bdbddef07958315d5094d7069e22
Aymeric Jakubczyk <ajakubcz@z4r12p3.42lyon.fr>
2024-02-06 17:36:50 +0100

    clean user

--- sources/User.cpp
+++ sources/User.cpp
@@ -3,132 +3,49 @@
 #include <algorithm>
 
 
+/*============================== CONSTRUCTOR ================================*/
+User::User(void):  _level(0) {}
 
-User::User(void):  _level(0)
-{
-}
-
-User::User(int fd) :_fd(fd), _isCreate(false), _level(0)
-{
-	return ;
-}
-
-User::~User(void) {
-	return;
-}
-
-User &	User::operator=(const User &obj){
-	this->_nickname = obj._nickname;
-	this->_username = obj._username;
-	this->_fd = obj._fd;
-	this->_isCreate = obj._isCreate;
-	return (*this);
-}
+User::User(int fd) :_fd(fd), _level(0) {}
 
 
-/*- - - - - - - - - - - - - - - - - GETTERS - - - - - - - - - - - -- - -  - - */
-const int& User::getFd(void) const
-{
-	return (this->_fd);
-}
+/*================================= GETTERS =================================*/
+std::string User::getNickname(void) const {return (this->_nickname);}
 
-const bool& User::getIsCreate(void) const
-{
-	return (this->_isCreate);
-}
+std::string User::getUsername(void) const {return (this->_username);}
 
-std::string User::getoldNickname(void) const
-{
-	return (this->_oldNickname);
-}
+std::string User::getoldNickname(void) const {return (this->_oldNickname);}
 
-std::vector<std::string> &User::getUserChannels(void)
-{
-	return (this->_userChannels);
-}
+const int& User::getFd(void) const {return (this->_fd);}
 
-std::vector<std::string> User::getInvite(void) const
-{
-	return (this->_invite);
-}
+int User::getLevel(void) const {return (this->_level);}
 
-int User::getLevel(void) const
-{
-	return (this->_level);
-}
+std::string User::getLine(void) const {return (this->_line);}
 
-std::string User::getLine(void) const
-{
-	return (this->_line);
-}
+std::vector<std::string> &User::getUserChannels(void) {return (this->_userChannels);}
+
+std::vector<std::string> User::getInvite(void) const {return (this->_invite);}
 
 
-std::string User::getNickname(void) const
-{
-	return (this->_nickname);
-}
+/*================================= SETTERS =================================*/
+void User::setNickname(std::string nickname) {this->_nickname = nickname;}
 
-std::string User::getUsername(void) const
-{
-	return (this->_username);
-}
+void User::setUsername(std::string username) {this->_username = username;}
 
-/*- - - - - - - - - - - - - - - - - SETTERS - - - - - - - - - - - -- - -  - - */
-void User::setIsCreate(bool isCreate)
-{
-	this->_isCreate = isCreate;
-	return ;
-}
+void User::setoldNickname(std::string oldNickname) {this->_oldNickname = oldNickname;}
 
-void User::setNickname(std::string nickname)
-{
-	this->_nickname = nickname;
-	return ;
-}
-
-void User::setUsername(std::string username)
-{
-	this->_username = username;
-	return ;
-}
-
-void User::setLine(std::string line)
-{
-	this->_line = line;
-	return ;
-}
-
-void User::addLevel()
-{
-	this->_level += 1;
-}
-
-void	User::addInvite(std::string channel_name) 
-{
-	this->_invite.push_back(channel_name);
-}
+void User::setLine(std::string line) {this->_line = line;}
 
 
-void User::setoldNickname(std::string oldNickname)
-{
-	this->_oldNickname = oldNickname;
-	return ;
-}
+/*=================================== ADD ===================================*/
+void User::addLevel() {this->_level += 1;}
 
-std::ostream &operator<<(std::ostream &out, const User &Object) {
+void	User::addInvite(std::string channel_name) {this->_invite.push_back(channel_name);}
 
-	out << Object.getNickname() << " " << Object.getUsername() << " " << Object.getIsCreate() << std::endl;
-	return (out);
-}
+void User::addChannel(std::string name_channel) {this->_userChannels.push_back(name_channel);}
 
-void User::addChannel(std::string name_channel)
-{
-	// std::cout << "[TEST] name : " << name_channel << std::endl;
-	this->_userChannels.push_back(name_channel);
-	// std::cout << "add channel function" << std::endl;
-	// std::cout << "SIZE = " << this->_userChannels.size();
-}
 
+/*================================== DELETE =================================*/
 void User::deleteChannel(std::string name_channel)
 {
 	std::vector<std::string>::iterator ite = this->_userChannels.end();
