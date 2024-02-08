27023e9f6e6fd0409065cf6273f087aa258aa7fd
Aymeric Jakubczyk <ajakubcz@z4r11p7.42lyon.fr>
2024-02-08 20:11:26 +0100

    remove user to waitlist when they quit

--- sources/commands/quit.cpp
+++ sources/commands/quit.cpp
@@ -25,6 +25,12 @@ void Server::quit(std::string msg, int fd)
 		message.append("\r\n");
 		this->part(message, fd);
 	}
+	std::vector<std::string> userChannelInvite = user.getInvite();
+	for (it = userChannelInvite.begin(); it != userChannelInvite.end(); it++) 
+	{
+		Channel &channel = this->_lst_channel[*it];
+		channel.deleteUserToWaitlist(user);
+	}
 	std::cout << RED << "USER QUIT (" << fd << ")" << RESET << std::endl;
 	message = HEADER_CMD(user) + "QUIT";
 	if (cmd.size() == 2)
